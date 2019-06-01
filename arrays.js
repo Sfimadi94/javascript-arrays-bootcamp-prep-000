08e43c8d139e6a5a2a7bc10a444a80c0bd3eb4c8
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-01 22:59:31 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = [
   "snickers", "hundred grand", "kitkat", "skittles"]
   
-function addElementToBeginningOfArray(chocolateBars, 'cookies'){
+function addElementToBeginningOfArray(){
   chocolateBars = ['cookies', chocolateBars]
 }
