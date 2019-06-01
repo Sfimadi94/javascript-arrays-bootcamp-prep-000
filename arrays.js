85c64a4bb0719576b99b819219f12ddb3c0bc533
charming-reference-7654 <sydeffect1994@gmail.com>
2019-06-01 23:02:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = [
   "snickers", "hundred grand", "kitkat", "skittles"]
   
 function addElementToBeginningOfArray(){
-  return ['foo', ...chocolateBars]
+  return ['foo','1' ...chocolateBars]
 }
 
 function destructivelyAddElementToEndOfArray(){
